044293ebfef2fdf72b075eed923584f01dfde75b
John Tyree <johntyree@gmail.com>
2013-02-12 18:25:34 +0100

    Cleanup imports and remove embedded test.

--- thrust/iterator/repeat_iterator.h
+++ thrust/iterator/repeat_iterator.h
@@ -6,12 +6,7 @@
 /* Remove this header */
 #include <GNUC_47_compat.h>
 
-#include <iostream>
 #include <thrust/iterator/iterator_adaptor.h>
-#include <thrust/iterator/counting_iterator.h>
-#include <thrust/copy.h>
-#include <thrust/host_vector.h>
-
 
 namespace thrust
 {
@@ -20,12 +15,12 @@ namespace thrust
 template<typename Iterator>
   class repeat_iterator
     : public thrust::iterator_adaptor<
-        repeat_iterator<Iterator>, // the first template parameter is the name of the type we're creating
-        Iterator,                  // the second template parameter is the name of the iterator we're adapting
-        thrust::use_default,      // the third template parameter is the name of the iterator's value_type -- it's simply the function's result_type.
-        thrust::use_default,            // the fourth template parameter is the name of the iterator's system. use_default will use the same system as the base iterator.
-        thrust::use_default,            // the fifth template parameter is the name of the iterator's traversal. use_default will use the same traversal as the base iterator.
-        thrust::use_default  // the sixth template parameter is the name of the iterator's reference. Like value_type, it's simply the function's result_type.
+        repeat_iterator<Iterator>, // name of the type we're creating
+        Iterator,                  // name of the iterator we're adapting
+        thrust::use_default,       // name of the iterator's value_type
+        thrust::use_default,       // name of the iterator's system
+        thrust::use_default,       // name of the iterator's traversal
+        thrust::use_default        // name of the iterator's reference
       >
 {
   public:
@@ -39,50 +34,36 @@ template<typename Iterator>
       thrust::use_default
     > super_t;
 
-    // the number of repeats
-    unsigned int rep;
-
-    const Iterator begin;
-
     __host__ __device__
-    repeat_iterator(const Iterator &x, int rep) : super_t(x), begin(x), rep(rep) {}
+    repeat_iterator(const Iterator &x, int n) : super_t(x), begin(x), n(n) {}
 
     // befriend thrust::iterator_core_access to allow it access to the private interface below
     friend class thrust::iterator_core_access;
 
   private:
+    // Repeat each element n times
+    unsigned int n;
+
+    // Used to compute a 'starting' point.
+    const Iterator begin;
+
     // it is private because only thrust::iterator_core_access needs access to it
     __host__ __device__
     typename super_t::reference dereference() const {
-      return *(begin + (this->base() - begin) / rep);
+      return *(begin + (this->base() - begin) / n);
     }
 
 };
 
 template<typename T>
 __host__ __device__
-repeat_iterator<T> make_repeat_iterator(T x, int rep)
+repeat_iterator<T> make_repeat_iterator(T x, int n)
 {
-    repeat_iterator<T> a(x, rep);
+    repeat_iterator<T> a(x, n);
     return a;
 }
 
 } // end thrust
 
-
-// int main() {
-    // thrust::host_vector<double> v(0);
-    // v.push_back(4);
-    // v.push_back(3);
-    // v.push_back(2);
-    // v.push_back(1);
-
-    // thrust::copy(make_repeat_iterator(v.begin(), 9),
-            // make_repeat_iterator(v.begin()+v.size()*9, 9),
-            // std::ostream_iterator<double>(std::cout, " "));
-    // // Desired output: 4 4 4 3 3 3 2 2 2 1 1 1
-
-    // return 0;
-// }
 
 #endif // THRUST_REPEAT_ITERATOR_H
